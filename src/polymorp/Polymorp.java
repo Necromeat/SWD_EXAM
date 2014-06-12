4132cab29909e6c03247d6461d9132747cc61c46
Andrew <Andrew@Home1>
2014-06-12 12:01:44 +0200

    Poly

--- src/polymorp/Polymorp.java
+++ src/polymorp/Polymorp.java
@@ -10,6 +10,7 @@ import cars.Car;
 import cars.TestVolvo;
 import interfaces.Vehicle;
 import java.util.ArrayList;
+import java.util.List;
 import trucks.Truck;
 import vans.Van;
 
@@ -17,11 +18,15 @@ import vans.Van;
  *
  * @author Andrew
  */
-public class Polymorp {
-    private ArrayList<Car> cars = new ArrayList<>();
-    ArrayList<Truck> trucks = new ArrayList<>();
-    ArrayList<Van> vans = new ArrayList<>();
-    ArrayList<Vehicle> vehicles = new ArrayList<>();
+public class Polymorp<T> {
+    private static ArrayList<Car> cars = new ArrayList<>();
+    private static ArrayList<Truck> trucks = new ArrayList<>();
+   private static  ArrayList<Van> vans = new ArrayList<>();
+    private static ArrayList<Vehicle> vehicles = new ArrayList<>();
+    List<List<? extends Vehicle>> test = new ArrayList<>();
+    private static  List<? super Vehicle> tests = new ArrayList<>();
+    List tester = new ArrayList();      
+
     Car c = new Car(1,2,3,"Ford1");
         Car c1 = new Car(3,4,5,"Ford2");
         Car c2 = new Car(5,6,7,"Ford3");
@@ -32,6 +37,7 @@ public class Polymorp {
         Truck t4 = new Truck(10,11,12,"Scania4");
         Van v1 = new Van(13,14,15,16,17,"Ford Van");
         TestVolvo tv = new TestVolvo(c1);
+        
     
     /**
      * @param args the command line arguments
@@ -42,9 +48,22 @@ public class Polymorp {
        p.truckFun();
        p.vanFun();
        p.VehicleFun();
-       
+      
+      //Crap
       p.print();
-        
+      //Generic
+      p.printall(cars);
+      p.printall(vans);
+      p.printall(trucks);
+      p.printall(tests);
+     
+      //Type
+      p.printalls(cars);
+      p.printalls(vans);
+      p.printalls(trucks);
+      p.printalls(tests);
+      
+      p.crapo();
     }
     
     
@@ -53,7 +72,9 @@ public class Polymorp {
         cars.add(c);
         cars.add(c2);
         cars.add(c3);
-        
+//        cars.add(tv);
+        tests.add(tv);
+        tests.add(c);
     }
     
     public void truckFun(){
@@ -78,6 +99,7 @@ public class Polymorp {
         vehicles.add(t3);
         vehicles.add(t4);
         vehicles.add(v1);
+        vehicles.add(tv);
     }
     
     public void print(){
@@ -97,5 +119,38 @@ public class Polymorp {
         
         System.out.println(tv.getType());
         tv.print();
+    }
+    
+    public void printall(List<? extends Vehicle> vs) {
+        System.out.println("Printing List ?");
+        test.add(vs);
+        for (Vehicle v: vs) {
+        System.out.println(v.getType());
+        }
+        
+    }
+    
+    public void printalls(List<T> vs) {
+        System.out.println("Printing List T");
+        
+    for (Object v: vs) {
+        
+        System.out.println(v.toString());
+        }
+    }
+    
+    public void crapo(){
+        System.out.println("Crapo");
+        tester.add(c);
+        System.out.println(tester.get(0).toString());
+        Car c1 = (Car)tester.get(0);
+        System.out.println(c1.getType());
+    }
+     public void pt(List<? super Object> vs) {
+        System.out.println("Printing List ?");
+       
+        for (Object v: vs) {
+        System.out.println(v.toString());
+        }
     }
 }
